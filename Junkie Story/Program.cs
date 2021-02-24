ba21ae9eca87b0d31694acea860eaf18dea77519
NaughtyCola <79594555+NaughtyCola@users.noreply.github.com>
2021-02-24 17:42:36 +0100

    Add files via upload

---
+++ Junkie Story/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace penis_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Scary Junkie";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Junkie: What Is Your Name?");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            String playerName = Console.ReadLine();
+            
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Junkie: Wanna See Something? " + playerName);
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("You: Yeah sure");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'               `$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+            Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$$$$'                   `$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+            Console.WriteLine("$$$'`$$$$$$$$$$$$$'`$$$$$$!                       !$$$$$$'`$$$$$$$$$$$$$'`$$$");
+            Console.WriteLine("$$$$  $$$$$$$$$$$  $$$$$$$      .           .      $$$$$$$  $$$$$$$$$$$  $$$$");
+            Console.WriteLine("$$$$. `$' ' --$`  $$$$$$$!                         !$$$$$$$  '$/ `/ `$' .$$$$");
+            Console.WriteLine("$$$$$. !-  i  i .$$$$$$$$                           $$$$$$$$. i  i  /! .$$$$$");
+            Console.WriteLine("$$$$$$   `--`--.$$$$$$$$$                           $$$$$$$$$.--'--'   $$$$$$");
+            Console.WriteLine("$$$$$$L        `$$$$$^^$$                           $$^^$$$$$'        J$$$$$$");
+            Console.WriteLine("$$$$$$$.   .'  ----   $$$    $.                 .$  $$$   ~--   `.   .$$$$$$$");
+            Console.WriteLine("$$$$$$$$.  ;      .e$$$$$!    $$.             .$$  !$$$$$e,      ;  .$$$$$$$$");
+            Console.WriteLine("$$$$$$$$$   `.$$$$$$$$$$$$     $$$.         .$$$   $$$$$$$$$$$$.'   $$$$$$$$$");
+            Console.WriteLine("$$$$$$$$    .$$$$$$$$$$$$$!     $$`$$$$$$$$'$$    !$$$$$$$$$$$$$.    $$$$$$$$");
+            Console.WriteLine("$$$$$$$     $$$$$$$$$$$$$$$$.    $    $$    $   .$$$$$$$$$$$$$$$$     $$$$$$$");
+            Console.WriteLine("                                 $    $$    $");
+            Console.WriteLine("                                 $.   $$   .$");
+            Console.WriteLine("                                 `$        $'");
+            Console.WriteLine("                                  `$$$$$$$$'");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("You: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHH");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("THE END");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
